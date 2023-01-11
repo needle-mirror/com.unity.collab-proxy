ea5aeb7b63c9dc8ee96b421c90b40672d406bc4c
Unity Technologies <@unity>
2023-01-11 00:00:00 +0000

    com.unity.collab-proxy@2.0.0 ## [2.0.0] - 2023-01-11
    Plastic SCM for Unity is now available as part of the Version Control Package! You can enable Plastic SCM via Window > Plastic SCM to get started!
    If you have previously used the Unity Asset Store Plastic SCM plug-in, you can now simply use this package. Make sure you delete the plug-in from your project.
    Removing a previously added Plastic SCM Asset Store Plug-In:
    - Select the PlasticSCM folder in the Assets\Plugins folder on the Project tab, then click Edit > Delete
    - Close the Unity Editor and open your project again. You will find the Plastic SCM menu item in the Window menu.
    
    ### Changed
    - Removed Collab from the package

--- .Toolbar/assets/entry.js
+++
@@ -1,9 +0,0 @@
-var entryPoint = function (functionName, param) {
-    if (window.angular &&
-        angular.element &&
-        angular.element(document.getElementById('collab-toolbar')).injector() &&
-        angular.element(document.getElementById('collab-toolbar')).injector().get('$rootScope')) {
-        // function call
-        angular.element(document.getElementById('collab-toolbar')).injector().get('$rootScope')[functionName](param);
-    }
-};
--- Editor/Client/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.CollabProxy.Client.Tests")]
-[assembly: InternalsVisibleTo("Unity.CollabProxy.UI")]
